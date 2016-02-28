9c735fdfedbbede017d4accacd59696b37864ec2
Gil <Gil@zion.home>
2016-02-28 17:58:39 +0000

    erro de falta de password (-10 no validate) tratamento desse erro pelo server (inacabado)

--- SC1/src/domain/server/myWhatsServer.java
+++ SC1/src/domain/server/myWhatsServer.java
@@ -1,10 +1,10 @@
 package domain.server;
 
 /***************************************************************************
-*  
-*
-*
-***************************************************************************/
+ *  
+ *
+ *
+ ***************************************************************************/
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -16,7 +16,7 @@ import java.util.HashMap;
 //Servidor do servico myWhatsServer
 
 public class myWhatsServer{
-	
+
 	private HashMap <String,User> userMap;
 	private HashMap <String,Group> groupMap;
 
@@ -28,33 +28,31 @@ public class myWhatsServer{
 
 	public void startServer (){
 		ServerSocket sSoc = null;
-        
+
 		try {
 			sSoc = new ServerSocket(23456);
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 			System.exit(-1);
 		}
-		
+
 		userMap = new HashMap <> ();
 		groupMap = new HashMap <> ();
-         
+
 		while(true) {
 			try {
 				Socket inSoc = sSoc.accept();
 				ServerThread newServerThread = new ServerThread(inSoc);
 				newServerThread.start();
-		    }
-		    catch (IOException e) {
-		        e.printStackTrace();
-		    }
-		    
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 		//sSoc.close();
 	}
-	
-	//flag da password fica a true se houver pass
-	private static boolean pass = false;
+
 	private static String flags = "-p-m-f-r-a-d";
 	private static StringBuilder y;
 	//onde eu quiser
@@ -65,11 +63,12 @@ public class myWhatsServer{
 	//-5 flag invalida a seguir ao -p
 	//-6 ordem errada da flag
 	//-7 argumentos das flag invalidos
+	//-10 falta password
 	public static int validate (String [] x){
 		y = new StringBuilder ();
 		if (x == null)
 			return -1;
-		
+
 		int size = x.length;
 
 		for (int i = 3; i < size; i++){
@@ -83,19 +82,18 @@ public class myWhatsServer{
 					}
 				}else
 					return -4;
-							
+
 				if(i+2 < size)
 					if(!flags.contains(x[i+2]))
 						return -5;
 				y.append('p');
-				pass = true;
 				break;
 			case "-m":
 				if (y.toString().contains("m") || 
-					y.toString().contains("f") ||
-					y.toString().contains("r") ||
-					y.toString().contains("a") ||
-					y.toString().contains("d"))
+						y.toString().contains("f") ||
+						y.toString().contains("r") ||
+						y.toString().contains("a") ||
+						y.toString().contains("d"))
 					return -6;
 				if(!arguments(i, x, size))
 					return -7;
@@ -103,9 +101,9 @@ public class myWhatsServer{
 				break;
 			case "-f":
 				if ( y.toString().contains("f") ||
-					 y.toString().contains("r") ||
-					 y.toString().contains("a") ||
-					 y.toString().contains("d"))
+						y.toString().contains("r") ||
+						y.toString().contains("a") ||
+						y.toString().contains("d"))
 					return -6;
 				if(!arguments(i, x, size))
 					return -7;
@@ -113,14 +111,14 @@ public class myWhatsServer{
 				break;
 			case "-r":
 				if (y.toString().contains("r") ||
-					y.toString().contains("a") ||
-					y.toString().contains("d"))
+						y.toString().contains("a") ||
+						y.toString().contains("d"))
 					return -6;
 				y.append('r');
 				break;
 			case "-a":
 				if (y.toString().contains("a") ||
-					y.toString().contains("d"))
+						y.toString().contains("d"))
 					return -6;
 				if(!arguments(i, x, size))
 					return -7;
@@ -134,21 +132,23 @@ public class myWhatsServer{
 				y.append('d');
 				break;
 			}
-	}			
+		}
+		if (!y.toString().contains("p"))
+			return -10;
 		return 1;
 	}		
-	
+
 
 	private static boolean arguments(int i , String [] args, int size){
 		if(i+2 >= size)
 			return false;
 		if (flags.contains(args[i+1]) || flags.contains(args[i+2]))
 			return false;
-		
+
 		if(i+3 < size)
 			if(!flags.contains(args[i+3]))
 				return false;
-		
+
 		return true;
 	}
 
@@ -162,32 +162,54 @@ public class myWhatsServer{
 			socket = inSoc;
 			System.out.println("thread do server para cada cliente");
 		}
- 
+
 		public void run(){
 			try {
 				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
 
 				String [] args = null;
-			
+
 				try {
 					args = (String [])inStream.readObject();
 					System.out.println("thread: depois de receber os args");
 				}catch (ClassNotFoundException e1) {
 					e1.printStackTrace();
 				}
- 			
-				//TODO: refazer
-				//este codigo apenas exemplifica a comunicao entre o cliente e o servidor
-				//nao faz qualquer tipo de autenticao
-			
-				outStream.writeObject(validate(args));
 				
-				
+				int test = validate(args);
+				outStream.writeObject(test);
+
+				//verifica se nao existe password
+				if (test == -10){
+					outStream.writeObject(-10);
+					//verifica se o hashmap contem o utilizador indicado
+					if (!userMap.containsKey(args[1])){
+
+					}
+					else{
+
+					}
+				}
+				else{
+					if (!userMap.containsKey(args[1])){
+						userMap.put(args[1], new User (args[1],args[4]));
+					}
+					//fez o login bem sucedido
+					else if (args[4].equals(userMap.get(args[1]).getPass())){
+
+					}
+					//informa que a pass esta incorrecta
+					else{
+
+					}
+				}
+
+
 
 				outStream.close();
 				inStream.close();
- 			
+
 				socket.close();
 
 			} catch (IOException e) {
