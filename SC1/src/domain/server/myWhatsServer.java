cf41987dcf6212623697d5bafd1194180f8827c6
-Zacky <-Zacky@192.168.1.32>
2016-02-29 21:06:48 +0000

    Pequena atualizacao no server, receber streams dentro de try Catch e preparar as excepcoes desse mesmo try catch.

--- SC1/src/domain/server/myWhatsServer.java
+++ SC1/src/domain/server/myWhatsServer.java
@@ -11,7 +11,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 
 //Servidor do servico myWhatsServer
 
@@ -25,6 +24,7 @@ public class myWhatsServer{
 		server.startServer();
 	}
 
+	@SuppressWarnings("resource")
 	public void startServer (){
 		ServerSocket sSoc = null;
 
@@ -68,38 +68,41 @@ public class myWhatsServer{
 			try {
 				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-
-				String [] args = null;
-
+				int numArgs;
+				String username;
 				try {
-					args = (String [])inStream.readObject();
+					numArgs = (int) inStream.readObject();
+					username = (String) inStream.readObject();
+					
+
+					//Validar e confirmar que se encontra tudo correcto
+					
+					
+					String aux;
+					//recepcao de parametros do client
+					for(int i = 0; i < numArgs; i++){
+						aux = (String) inStream.readObject();
+						switch (aux) {
+						case "-p":
+							skell.authenticate((String)inStream.readObject(), username);
+							//CONTINUAR POR AQUI!!
+						}
+					}
+					
 					System.out.println("thread: depois de receber os args");
 				}catch (ClassNotFoundException e1) {
 					e1.printStackTrace();
 				}
 				
-				int numArgs = (int) inStream.readObject();
-				String username = (String) inStream.readObject();
 				
-				//Validar e confirmar que se encontra tudo correcto
-				
-				
-				String aux;
-				//recepcao de parametros do client
-				for(int i = 0; i < numArgs; i++){
-					aux = (String) inStream.readObject();
-					switch (aux) {
-					case "-p":
-						skell.authenticate((String)inStream.readObject(), username);
-					}
-				}
+				outStream.writeObject(0);
 				
 				outStream.close();
 				inStream.close();
 
 				socket.close();
 
-			} catch (IOException | ClassNotFoundException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
